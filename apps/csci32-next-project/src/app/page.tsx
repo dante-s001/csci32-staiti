48791dfe1302613d2b387bff34aa6a594c0e4de9
dante-s001 <dstaiti001@student.butte.edu>
2025-09-08 22:18:35 -0700

    removed unnecessary package-lock.json

--- apps/csci32-next-project/src/app/page.tsx
+++ apps/csci32-next-project/src/app/page.tsx
@@ -6,14 +6,8 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
+          <li className="mb-2 tracking-[-.01em]">Welcome to my website!</li>
+          <li className="tracking-[-.01em]">I don&apos;t know what to call it yet, but I will get there soon.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
