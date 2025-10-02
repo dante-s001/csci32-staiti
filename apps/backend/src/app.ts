338f2074dbda8ba86910b32333dd0c21bfd35dea
dante-s001 <dstaiti001@student.butte.edu>
2025-10-02 13:36:55 -0700

    Prisma Schema Not Working

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -5,6 +5,7 @@ import Fastify from 'fastify'
 import { fileURLToPath } from 'node:url'
 import autoLoad from '@fastify/autoload'
 import { dirname, join } from 'node:path'
+import { registerGraphQL } from './utils/graphql'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
