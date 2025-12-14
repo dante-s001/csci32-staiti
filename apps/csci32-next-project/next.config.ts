eb07ee40f5f58f4d3d4a1bd4cc69fc9ccefbb994
dante-s001 <dstaiti001@student.butte.edu>
2025-12-14 15:20:16 -0800

    attempted transpile fix for vercel

--- apps/csci32-next-project/next.config.ts
+++ apps/csci32-next-project/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  transpilePackages: ['@repo/ui'],
+}
 
-export default nextConfig;
+export default nextConfig
